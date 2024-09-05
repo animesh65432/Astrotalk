9c77484f85a6c0468075e131f65ee8be95d90e48
animesh65432 <kiranduttta234@gmail.com>
2024-09-05 13:13:53 +0530

    connect with database

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    DATABASEURL: process.env.DATABASEURL,
+  },
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+"use client";
+
+import { useEffect } from "react";
+import dbconnect from "@/lib/dbConnect";
+
 export default function Home() {
+  const checkdatabaseconnection = async () => {
+    try {
+      await dbconnect();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    checkdatabaseconnection();
+  }, []);
   return <></>;
 }
